709c0b4c947bda1e85f25154b00e8fcd90790dd4
Domovikx <domovikx@gmail.com>
2019-02-15 17:35:55 +0300

    Обычное обновление

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -7,25 +7,20 @@
 // Пузырьковая сортировка
 
 let array = [4, 2, 6, 3, 7, 3, 1, 0, 4, 9, 10, 3];
-console.log('массив до сортировки ' + array);
-console.log('массив после :', bubbleSort(array));
 
+bubbleSort(array)
 function bubbleSort(array) {
-
+    // console.log('array :', array);
     let length = array.length; // длина массива
-
     for (let i = 0; i < length - 1; i++) {
-
         for (let j = 1; j < length; j++) {
-
             if (array[j - 1] > array[j]) {
                 let temp = array[j - 1];
                 array[j - 1] = array[j];
                 array[j] = temp;
             }
-
         }
-        console.log(array);
+        //console.log(array);
     }
     return array;
 }
--- sorting/shakerSort.js
+++ sorting/shakerSort.js
@@ -7,7 +7,7 @@ let array = [4, 2, 6, 3, 7, 3, 1, 0, 4, 9, 10, 3];
 
 shakerSort(array);
 function shakerSort(array) {
-    console.log('first array :', array);
+    // console.log('first array :', array);
 
     let left = 0; //начало массива
     let right = array.length - 1; //конец массива
@@ -22,7 +22,7 @@ function shakerSort(array) {
                 array[i + 1] = temp;
             }
         }
-        console.log('array :', array);
+        // console.log('array :', array);
         right--;
 
         for (let i = right; left < i; i--) {
@@ -33,10 +33,10 @@ function shakerSort(array) {
                 array[i - 1] = temp;
             }
         }
-        console.log('array :', array);
+        //console.log('array :', array);
         left++;
 
     } while (left < right);
-
-console.log('array :', array);
+return array;
+// console.log('array :', array);
 }
