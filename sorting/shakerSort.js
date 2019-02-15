757b3fd306e753263e013eca1a97e5c9131ba040
Domovikx <domovikx@gmail.com>
2019-02-15 16:33:07 +0300

    Разбор завалов
    - Разобрал файлы по своим папкам
    - Добавил сортировку пузырьковую и шейкер

---
+++ sorting/shakerSort.js
@@ -0,0 +1,42 @@
+
+// Шейкер сортировка или коктель сортировка
+// не верь википедии! Она врет! 
+// бери код с JAVA и переноси логику
+
+let array = [4, 2, 6, 3, 7, 3, 1, 0, 4, 9, 10, 3];
+
+shakerSort(array);
+function shakerSort(array) {
+    console.log('first array :', array);
+
+    let left = 0; //начало массива
+    let right = array.length - 1; //конец массива
+    let temp; //буфер
+
+    do {
+        for (let i = left; i < right; i++) {
+
+            if (array[i] > array[i + 1]) {
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+        console.log('array :', array);
+        right--;
+
+        for (let i = right; left < i; i--) {
+
+            if (array[i] < array[i - 1]) {
+                temp = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = temp;
+            }
+        }
+        console.log('array :', array);
+        left++;
+
+    } while (left < right);
+
+console.log('array :', array);
+}
